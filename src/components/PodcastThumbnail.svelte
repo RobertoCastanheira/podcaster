62a93434d5f2522b3ce1b4a6e246cfda9934bad4
RobertoCastanheira <roberto.castanheira@hydradev.io>
2023-02-23 16:11:08 +0000

    Main view with podcasts. Podcast thumbnail.

---
+++ src/components/PodcastThumbnail.svelte
@@ -0,0 +1,55 @@
+<script>
+  import { Link } from "svelte-routing";
+
+  export let podcast;
+</script>
+
+<Link to="/podcast/{podcast.id}">
+  <card>
+    <img src={podcast.image} alt={podcast.name} />
+    <div class="shadow">
+      <p class="name">{podcast.name}</p>
+      <span class="author">Author: {podcast.author}</span>
+    </div>
+  </card>
+</Link>
+
+<style>
+  card {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 10rem;
+    margin: 1rem;
+  }
+
+  img {
+    border-radius: 50%;
+    width: 5rem;
+    height: auto;
+    z-index: 1;
+    margin-bottom: -2rem;
+  }
+
+  .shadow {
+    box-shadow: 2px 2px 5px 0px rgba(0,0,0,0.75);
+    -webkit-box-shadow: 2px 2px 5px 0px rgba(0,0,0,0.75);
+    -moz-box-shadow: 2px 2px 5px 0px rgba(0,0,0,0.75);
+    padding: .5rem;
+    padding-top: 2.5rem;
+    text-align: center;
+    width: 100%;
+  }
+
+  .name {
+    font-size: .8rem;
+    margin-bottom: .1rem;
+    color: hsl(205deg, 16%, 77%);
+  }
+
+  .author {
+    margin-top: 0;
+    font-size: .6rem;
+    color: hsl(205deg, 16%, 77%);
+  }
+</style>
--- src/views/MainView.svelte
+++ src/views/MainView.svelte
@@ -1,10 +1,35 @@
-
 <script>
-  import { Link } from "svelte-routing";
-  import HeaderLayout from '../layouts/HeaderLayout.svelte';
+  import { onMount } from "svelte";
+  import { podcastList, fetchPodcasts } from "../stores/PodcastList.js";
+  import HeaderLayout from "../layouts/HeaderLayout.svelte";
+  import PodcastThumbnail from "../components/PodcastThumbnail.svelte";
+
+  let podcasts = [];
+  let loading;
+
+  onMount(async () => {
+    loading = true;
+    await fetchPodcasts();
+    podcastList.subscribe((value) => {
+      podcasts = value;
+      loading = false;
+    });
+  });
 </script>
 
-<HeaderLayout>
-  <Link to="/podcast/1">to: podcast details</Link>
-  <p>main view</p>
+<HeaderLayout loading={loading}>
+  <div class="podcasts">
+    {#each podcasts as podcast}
+      <PodcastThumbnail {podcast} />
+    {/each}
+  </div>
 </HeaderLayout>
+
+<style>
+  .podcasts {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 2rem;
+  }
+</style>
