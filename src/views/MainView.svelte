4864c94f12e153c00668ddfb5ee801aeb5cf9f88
RobertoCastanheira <roberto.castanheira@hydradev.io>
2023-02-23 19:31:06 +0000

    Filter podcasts

--- src/views/MainView.svelte
+++ src/views/MainView.svelte
@@ -6,6 +6,7 @@
 
   let podcasts = [];
   let loading;
+  $: filteredPodCasts = podcasts;
 
   onMount(async () => {
     loading = true;
@@ -15,17 +16,50 @@
       loading = false;
     });
   });
+
+  const filterPodcasts = (event) => {
+    const filter = event.target.value.toLowerCase()
+
+    filteredPodCasts = podcasts.filter((podcast) => {
+      if (podcast.name.toLowerCase().includes(filter)) return podcast;
+      if (podcast.author.toLowerCase().includes(filter)) return podcast;
+    })
+  }
 </script>
 
 <HeaderLayout loading={loading}>
+  <div class="search">
+    <div class="count">{filteredPodCasts.length}</div>
+    <input type="text" placeholder="Filter podcasts..." on:input={filterPodcasts}>
+  </div>
   <div class="podcasts container">
-    {#each podcasts as podcast}
+    {#each filteredPodCasts as podcast}
       <PodcastThumbnail {podcast} />
     {/each}
   </div>
 </HeaderLayout>
 
 <style>
+  .search {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    padding-right: 3rem;
+  }
+
+  input {
+    width: 10rem;
+    height: 2rem;
+    margin: 0;
+  }
+
+  .count {
+    width: 2rem;
+    text-align: center;
+    border-radius: 5px;
+    background-color: green;
+    margin-right: 1rem;
+  }
   .podcasts {
     width: 100%;
     display: flex;
