670d1cf749f390803717dda9d36962e2ecec4788
RobertoCastanheira <roberto.castanheira@hydradev.io>
2023-02-23 15:44:20 +0000

    Header component with loading state

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,8 +1,19 @@
 <script>
   import { Link } from "svelte-routing";
+  export let loading = false;
 </script>
 
 <header>
-  (header)
   <Link to="/">Podcaster</Link>
+  <span aria-busy={loading} />
 </header>
+
+<style>
+  header {
+    width: 100%;
+    border-bottom: 1px solid lightgray;
+    display: flex;
+    justify-content: space-between;
+    padding: 1rem;
+  }
+</style>
--- src/layouts/HeaderLayout.svelte
+++ src/layouts/HeaderLayout.svelte
@@ -1,6 +1,7 @@
 <script>
   import Header from '../components/Header.svelte';
+  export let loading = false;
 </script>
 
-<Header />
+<Header {loading}/>
 <slot />
