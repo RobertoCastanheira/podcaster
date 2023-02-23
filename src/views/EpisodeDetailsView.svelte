15a3f5c369c932166638e9fc45fb6b909d14c216
RobertoCastanheira <roberto.castanheira@hydradev.io>
2023-02-23 19:48:14 +0000

    Interpreted html for episode description

--- src/views/EpisodeDetailsView.svelte
+++ src/views/EpisodeDetailsView.svelte
@@ -25,7 +25,7 @@
 <LeftBarLayout {loading}>
   <div class="card shadow">
     <h1>{podcastDetails?.title}</h1>
-    <p>{episodeDetails?.description}</p>
+    <p>{@html episodeDetails?.description}</p>
     <audio class="audio" controls>
       <source src={episodeDetails?.episodeUrl} type="audio/mp3">
     </audio>
